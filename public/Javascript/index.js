90fd190d07f3a0712a76adae1aea6a1174e7748d
Gurpreet Singh <74554439+Gurpreet06@users.noreply.github.com>
2021-07-05 18:57:28 +0200

    SearchBarFixed

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -72,65 +72,6 @@ function init() {
 
 }
 
-let seachrBarTemp = ` <div class="searchInfo">
-<div class='searchRst'>
-    <h5 class="usrName">{{name}}</h5>
-    <p class='fileType'>Type: {{File}}</p>
-</div>
-</div>`
-
-let rstFound = `<p class='noFileType'>No File Found</p>`
-
-async function serachBar(searchValue) {
-    let Searchwrapper = document.getElementById('Searchwrapper')
-    let html = ''
-
-    let template = seachrBarTemp
-
-    if (searchValue != '') {
-        for (cnt = 0; cnt < dir.length; cnt = cnt + 1) {
-            let FolValue = dir[cnt]
-            if (FolValue.indexOf(searchValue) != -1) {
-                html = html + template
-                    .replaceAll(/{{name}}/g, dir[cnt])
-                    .replaceAll(/{{File}}/g, 'Folder')
-            }
-        }
-
-        for (cnt = 0; cnt < files.length; cnt = cnt + 1) {
-            let FolValue = files[cnt]
-            if (FolValue.indexOf(searchValue) != -1) {
-                html = html + template
-                    .replaceAll(/{{name}}/g, files[cnt])
-                    .replaceAll(/{{File}}/g, 'File')
-            }
-        }
-
-        for (cnt = 0; cnt < b.length; cnt = cnt + 1) {
-            let TaskVal = b[cnt]
-            if (TaskVal.name.indexOf(searchValue) != -1 || TaskVal.des.indexOf(searchValue) != -1) {
-                html = html + template
-                    .replaceAll(/{{name}}/g, b[cnt].name)
-                    .replaceAll(/{{File}}/g, 'Task')
-            }
-        }
-
-        for (cnt = 0; cnt < b.length; cnt = cnt + 1) {
-            let TaskVal = b[cnt]
-            if (TaskVal.des.indexOf(searchValue) != -1) {
-                html = html + template
-                    .replaceAll(/{{name}}/g, b[cnt].des)
-                    .replaceAll(/{{File}}/g, 'Task Desc')
-            }
-        }
-    }
-
-    Searchwrapper.style.display = 'block'
-    Searchwrapper.innerHTML = html
-    if (searchValue == '') {
-        Searchwrapper.style.display = 'none'
-    }
-}
 
 //Show Add event template
 async function showMenu() {
--- public/Javascript/time.js
+++ public/Javascript/time.js
@@ -9,7 +9,7 @@ let current = new Date()
 let time = current.getDate() + " " + months[current.getMonth()];
 refdate.innerHTML = time
 
-setInterval (function () {
+setInterval(function () {
     // Create Time
     let times = '';
     if (current.getMinutes() < 10) {
@@ -17,5 +17,68 @@ setInterval (function () {
     } else {
         times = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds()
     }
+
     refTime.innerHTML = times
 }, 1000);
+
+
+
+let seachrBarTemp = ` <div class="searchInfo">
+<div class='searchRst'>
+    <h5 class="usrName">{{name}}</h5>
+    <p class='fileType'>Type: {{File}}</p>
+</div>
+</div>`
+
+let rstFound = `<p class='noFileType'>No File Found</p>`
+
+async function serachBar(searchValue) {
+    let Searchwrapper = document.getElementById('Searchwrapper')
+    let html = ''
+
+    let template = seachrBarTemp
+
+    if (searchValue != '') {
+        for (cnt = 0; cnt < dir.length; cnt = cnt + 1) {
+            let FolValue = dir[cnt]
+            if (FolValue.indexOf(searchValue) != -1) {
+                html = html + template
+                    .replaceAll(/{{name}}/g, dir[cnt])
+                    .replaceAll(/{{File}}/g, 'Folder')
+            }
+        }
+
+        for (cnt = 0; cnt < files.length; cnt = cnt + 1) {
+            let FolValue = files[cnt]
+            if (FolValue.indexOf(searchValue) != -1) {
+                html = html + template
+                    .replaceAll(/{{name}}/g, files[cnt])
+                    .replaceAll(/{{File}}/g, 'File')
+            }
+        }
+
+        for (cnt = 0; cnt < b.length; cnt = cnt + 1) {
+            let TaskVal = b[cnt]
+            if (TaskVal.name.indexOf(searchValue) != -1 || TaskVal.des.indexOf(searchValue) != -1) {
+                html = html + template
+                    .replaceAll(/{{name}}/g, b[cnt].name)
+                    .replaceAll(/{{File}}/g, 'Task')
+            }
+        }
+
+        for (cnt = 0; cnt < b.length; cnt = cnt + 1) {
+            let TaskVal = b[cnt]
+            if (TaskVal.des.indexOf(searchValue) != -1) {
+                html = html + template
+                    .replaceAll(/{{name}}/g, b[cnt].des)
+                    .replaceAll(/{{File}}/g, 'Task Desc')
+            }
+        }
+    }
+
+    Searchwrapper.style.display = 'block'
+    Searchwrapper.innerHTML = html
+    if (searchValue == '') {
+        Searchwrapper.style.display = 'none'
+    }
+}
